f6cc1e369ec4f0296702ccafccbe04282dfb658e
KozhevnikovIvan <32384646+KozhevnikovIvan@users.noreply.github.com>
2017-10-16 15:08:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 int main(int argc, char* argv[])
 {
-string str1 = argv[1];
-char symbhol = argv [2][0];
-for (int i=0; i< str1.length(); i++)
-{
-        if (str1[i]==symbhol)
-        cout<<i+1<<endl;
-} 
+        string str1 = argv[1];
+        char symbhol = argv [2][0];
+                for (int i=0; i< str1.length(); i++)
+                {
+                        if (str1[i]==symbhol)
+                        cout<<i+1<<endl;
+                } 
 }
