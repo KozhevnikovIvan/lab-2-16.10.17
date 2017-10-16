61e9aa7ad9ad0495b97554957a97133b652d2444
KozhevnikovIvan <32384646+KozhevnikovIvan@users.noreply.github.com>
2017-10-16 14:36:08 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+string str1 = argv[1];
+char symbhol = argv [2][0];
+for (int i=0; i< str1.length(); i++)
+{
+        if (str1[i]==symbhol)
+        cout<<i+1<<endl;
+} 
+}
